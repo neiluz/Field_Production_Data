0ee3a0dcf3866dc0cbcc82bc522c76307f10f06a
Neivys Luz <41589719+neiluz@users.noreply.github.com>
2024-01-22 00:29:59 +0100

    Update reservoir.py

--- reservoir.py
+++ reservoir.py
@@ -23,7 +23,7 @@ st.image("linkedin_banner.png", use_column_width=True)
 st.subheader("Production Wells Analysis")
 
 #load data
-df_production = pd.read_excel('Volve production data.xlsx')
+df_production = pd.read_excel('Volve production data.xlsx', sheet_name='Sheet1')
 
 # Procesamiento de datos
 
